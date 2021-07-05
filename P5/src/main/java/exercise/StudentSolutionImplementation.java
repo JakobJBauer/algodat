8222bf266cf2a8c6d320d6db526dddb14f256273
Jakob Johannes Bauer <53357351+JakobJBauer@users.noreply.github.com>
2021-07-06 00:52:28 +0200

    Fixed bugs

--- P5/src/main/java/exercise/StudentSolutionImplementation.java
+++ P5/src/main/java/exercise/StudentSolutionImplementation.java
@@ -4,6 +4,8 @@ import main.java.framework.StudentInformation;
 import main.java.framework.StudentSolution;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class StudentSolutionImplementation implements StudentSolution {
     @Override
@@ -17,70 +19,76 @@ public class StudentSolutionImplementation implements StudentSolution {
 
     // Implementieren Sie hier Ihre Lösung mit Polynomialzeitreduktion
     public int findMaxClique(Graph g, TimedSolver solver, boolean[] chosenVertices) {
-        int k_lower = 0, k_upper = g.numberOfVertices(), k;
+        int k_lower = 0, k_upper = g.numberOfVertices() - 1, k;
         String solution = "";
 
-        while (k_lower != k_upper) {
-            k = (k_upper - k_lower) / 2;
+        while (k_lower <= k_upper) {
+            k = (k_upper + k_lower) / 2;
             String dimacs = dimacsFromGraph(g, k);
-            if ((solution = solver.solve(dimacs)).equals("")) k_upper = k;
-            else k_lower = k;
+            if (solver.solve(dimacs).equals("")) k_upper = k-1;
+            else {
+                    k_lower = k+1;
+                    solution = solver.solve(dimacs);
+            }
         }
 
-        choseVertices(solution, chosenVertices);
+        choseVertices(solution, chosenVertices, g.numberOfVertices());
         return numberOfChosenVertices(chosenVertices);
     }
 
     private String dimacsFromGraph(Graph g, int k) {
-        int n_var = 0, n_claus = 0;
+        int n_claus = 0;
         StringBuilder dimacs = new StringBuilder();
 
-        // https://cs.stackexchange.com/questions/70531/reduction-3sat-and-clique
         // Some node is the rth node of the clique
         for (int r = 1; r <= k; r++) {
             for (int i = 1; i <= g.numberOfVertices(); i++) {
-                int index = g.numberOfVertices() * (r - 1) + i;
+                int index = encode(g.numberOfVertices(), r, i);
                 dimacs.append(index).append(" ");
             }
             dimacs.append(0).append("\n");
+            n_claus++;
         }
 
         // No node is both, the rth and the sth node of the clique
-        for (int s = 1; s <= k; s++) {
-            for (int r = 1; r < s; r++) {
-                for (int i = 1; i <= g.numberOfVertices(); i++) {
-                    int indexR = g.numberOfVertices() * (r - 1) + i;
-                    int indexS = g.numberOfVertices() * (s - 1) + i;
-                    dimacs.append(-indexR).append(" ").append(-indexS).append(" ");
+        for (int i = 1; i <= g.numberOfVertices(); i++) {
+            for (int r = 1; r <= k; r++) {
+                for (int s = r + 1; s <= k; s++) {
+                    int indexR = encode(g.numberOfVertices(), r, i);
+                    int indexS = encode(g.numberOfVertices(), s, i);
+                    dimacs.append(-indexR).append(" ").append(-indexS).append(" ").append(0).append("\n");
+                    n_claus++;
                 }
-                dimacs.append(0).append("\n");
             }
         }
 
         // If there's no edge from i to j then nodes i and j cannot both be in the clique
-        for (int s = 1; s <= k; s++) {
-            for (int r = 1; r <= k && r != s; r++) {
-                for (int j = 1; j <= g.numberOfVertices() ; j++) {
-                    for (int i = 1; i < j; i++) {
-                        int indexIR = g.numberOfVertices() * (r - 1) + i;
-                        int indexJS = g.numberOfVertices() * (s - 1) + j;
+        for (int r = 1; r <= k; r++) {
+            for (int s = 1; s <= k; s++) {
+                if (r == s) continue;
+                for (int i = 1; i <= g.numberOfVertices(); i++) {
+                    for (int j = i + 1; j <= g.numberOfVertices(); j++) {
                         if (!g.containsEdge(i, j)) {
-                            dimacs.append(-indexIR).append(" ").append(-indexJS).append(" ");
+                            int indexIR = encode(g.numberOfVertices(), r, i);
+                            int indexJS = encode(g.numberOfVertices(), s, j);
+                            dimacs.append(-indexIR).append(" ").append(-indexJS).append(" ").append(0).append("\n");
+                            n_claus++;
                         }
                     }
                 }
-                dimacs.append(0).append("\n");
             }
         }
 
-        return String.format("p cnf %d %d\n", n_var, n_claus) + dimacs;
+        return String.format("p cnf %d %d\n", g.numberOfVertices() * k, n_claus) + dimacs;
     }
 
-    private void choseVertices(String DIMACSSolution, boolean[] chosenVertices) {
+    private void choseVertices(String DIMACSSolution, boolean[] chosenVertices, int n) {
         for (String dimac : DIMACSSolution.split(" ")) {
-            int index = Integer.parseInt(dimac);
-            if (index == 0) return;
-            chosenVertices[Math.abs(index)-1] = index > 0;
+            if (dimac.equals("0")) return;
+            if (!dimac.startsWith("-")) {
+                int index = decode(Integer.parseInt(dimac), n);
+                chosenVertices[index-1] = true;
+            }
         }
     }
 
@@ -88,5 +96,13 @@ public class StudentSolutionImplementation implements StudentSolution {
         int i = 0;
         for (boolean chosen : chosenVertices) i += chosen ? 1 : 0;
         return i;
+    }
+
+    private int decode(int encoded, int n) {
+        return 1 + ((encoded - 1) % n);
+    }
+
+    private int encode(int n, int i, int v) {
+        return n * (i - 1) + v;
     }
 }
