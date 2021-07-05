7df430c69ddf791d0eee2fad241e62139e05ff50
Jakob Johannes Bauer <53357351+JakobJBauer@users.noreply.github.com>
2021-07-06 00:53:01 +0200

    Bugfixes

--- P4/src/main/java/exercise/StudentSolutionImplementation.java
+++ P4/src/main/java/exercise/StudentSolutionImplementation.java
@@ -19,32 +19,66 @@ public class StudentSolutionImplementation implements StudentSolution {
 
     // Implementieren Sie hier Ihre Lösung für Verkettung der Überläufer
     public void insertVerkettung(HashTableWithChaining t, ChainElement chainElement, int m) {
-
+        int position = this.h1(chainElement.getKey(), m);
+        if (t.containsNoChainElement(position))
+            t.insertChainElement(chainElement, position);
+        else {
+            ChainElement currLast = t.get(position);
+            chainElement.setNext(currLast);
+            t.replaceChainElement(chainElement, position);
+        }
     }
 
     // Implementieren Sie hier Ihre Lösung für die lineare Sondierung
     public int linearesSondieren(int key, int i, int m) {
-        return 0;
+        return (h1(key, m) + i) % m;
     }
 
     // Implementieren Sie hier Ihre Lösung für die quadratische Sondierung
     public int quadratischesSondieren(int key, int i, int m) {
-        return 0;
+        return (int)(h1(key, m) + 0.5*i + 0.5*i*i) % m;
     }
 
     // Implementieren Sie hier Ihre Lösung für Double Hashing
     public int doubleHashing(int key, int i, int m) {
-        return 0;
+        return (h1(key, m) + i * h2(key)) % m;
     }
 
     // Implementieren Sie hier Ihre Lösung für die Insert-Methode
     public void insert(HashTable t, Probe p, int key, int m) {
-
+        for (int i = 0; ; i++){
+            int position = p.evaluate(key, i);
+            if (t.isFree(position)) {
+                t.insert(key, position);
+                break;
+            }
+        }
     }
 
     // Implementieren Sie hier Ihre Lösung für Verbesserung nach Brent
     public void insertVerbesserungNachBrent(HashTable t, int key, int m) {
+        int position = h1(key, m);
+        while (!t.isFree(position)) {
+            int key2 = t.get(position);
+            int position1 = (position + h2(key)) % m;
+            int position2 = (position + h2(key2)) % m;
+            if (t.isFree(position1) || !t.isFree(position2))
+                position = position1;
+            else {
+                t.replace(key, position);
+                key = key2;
+                position = position2;
+            }
+        }
+        t.insert(key, position);
+    }
 
+    private int h1(int key, int m) {
+        return key % m;
+    }
+
+    private int h2(int key) {
+        return 1 + (key % 5);
     }
 
 }
